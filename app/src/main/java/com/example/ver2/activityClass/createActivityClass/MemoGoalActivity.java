02e4244b93639a3a420ff88cf9b30fe67d9f5754
ggKamiKami <ggkamikami27@gmail.com>
2025-03-13 13:51:55 +0900

    refactoring MemoGoalActivity.java 2025/03/13

--- app/src/main/java/com/example/ver2/activityClass/createActivityClass/MemoGoalActivity.java
+++ app/src/main/java/com/example/ver2/activityClass/createActivityClass/MemoGoalActivity.java
@@ -1,14 +1,18 @@
+/*
+    自由記述形式を利用した目標設定を行うActivity
+    このActivityの後はSaveGoalActivityで目標を保存する（目標の名前、詳細、作成日、開始日、終了日、タスクを入力するActivity）
+ */
+
 package com.example.ver2.activityClass.createActivityClass;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
+//import androidx.fragment.app.FragmentManager;
+//import androidx.fragment.app.FragmentTransaction;
 
 import com.example.ver2.fragmentClass.BackConfirmListener;
 import com.example.ver2.R;
@@ -26,6 +30,7 @@ public class MemoGoalActivity extends AppCompatActivity implements BackConfirmLi
 
         memoEditText = findViewById(R.id.memo_goal_1_editText);
 
+        //SaveGoalActivityから戻ってきた際、前に入力した情報を取得する --> 目標設定中はその情報をこのクラスとSaveGoalActivityクラスで保持する
         Intent intent = getIntent();
         if (intent != null && intent.hasExtra("memo_goal")) {
             memoGoal = intent.getParcelableExtra("memo_goal");
@@ -34,34 +39,28 @@ public class MemoGoalActivity extends AppCompatActivity implements BackConfirmLi
             }
         }
 
+        //Memo_GoalオブジェクトをSaveGoalActivityに送り、Activity遷移を行う
         Button saveButton = findViewById(R.id.memo_goal_1_saveButton);
-
-        //saveボタンのリスナ
-        saveButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+        saveButton.setOnClickListener(view -> {
                 String memo = memoEditText.getText().toString();
-                //Memoオブジェクトを作成(name,descriptionなどはnullで初期化)
+                //始めの場合はここでインスタンス化する。二回目以降はMemo_Goalオブジェクトが保持してあるので、それを上書きする
                 if (memoGoal == null) {
                     memoGoal = new Memo_Goal(memo);
                 } else {
                     memoGoal.setMemo(memo);
                 }
                 //Intentを作成し、Memo_Goalオブジェクトをセット
-                Intent intent = new Intent(MemoGoalActivity.this, SaveGoalActivity.class);
-                intent.putExtra("memo_goal", memoGoal);
+                Intent intent_next = new Intent(MemoGoalActivity.this, SaveGoalActivity.class);
+                intent_next.putExtra("memo_goal", memoGoal);
                 //次のActivityを開始
-                startActivity(intent);
-            }
+                startActivity(intent_next);
         });
 
+        //何もしないで前のActivityに遷移
         Button backButton = findViewById(R.id.memo_goal_1_backButton);
-        backButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MemoGoalActivity.this, CreateGoalChooseFrameworkActivity.class);
-                startActivity(intent);
-            }
+        backButton.setOnClickListener(view -> {
+                Intent intent_before = new Intent(MemoGoalActivity.this, CreateGoalChooseFrameworkActivity.class);
+                startActivity(intent_before);
         });
     }
 
