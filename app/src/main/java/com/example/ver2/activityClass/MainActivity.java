5ed89203f7daf97c854e24806d4cea80b4e312fd
ggKamiKami227 <ggkamikami7@gmail.com>
2025-03-14 10:34:51 +0900

    refactoring MainActivity.java 2025/03/13

--- app/src/main/java/com/example/ver2/activityClass/MainActivity.java
+++ app/src/main/java/com/example/ver2/activityClass/MainActivity.java
@@ -1,8 +1,11 @@
+/*
+    アプリケーションのホーム画面
+ */
+
 package com.example.ver2.activityClass;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -16,31 +19,20 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //EdgeToEdge.enable(this);
         setContentView(R.layout.app_top);
-        /*ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });*/
 
+        //目的、目標を新しく作る
         Button createButton = findViewById(R.id.top_create_Button);
-        createButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+        createButton.setOnClickListener(view -> {
                 Intent intent = new Intent(MainActivity.this, CreateTopChooseActivity.class);
                 startActivity(intent);
-            }
         });
 
+        //目的、目標を確認する
         Button confirmButton = findViewById(R.id.top_confirm_Button);
-        confirmButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+        confirmButton.setOnClickListener(view -> {
                 Intent intent = new Intent(MainActivity.this, ConfirmTopChooseActivity.class);
                 startActivity(intent);
-            }
         });
-
     }
 }
