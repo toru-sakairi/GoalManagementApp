bfeb723c10bd978abc758f73f9ea802301a80c06
ggKamiKami227 <ggkamikami7@gmail.com>
2025-03-19 15:46:15 +0900

    Implementing a task deletion function. Implementing a delete button and delete method in RecyclerViewTaskListAdapter 2025/03/19

--- app/src/main/java/com/example/ver2/recyclerViewClass/RecyclerViewTaskListAdapter.java
+++ app/src/main/java/com/example/ver2/recyclerViewClass/RecyclerViewTaskListAdapter.java
@@ -3,12 +3,14 @@ package com.example.ver2.recyclerViewClass;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.ver2.R;
 import com.example.ver2.dataClass.Task;
+import com.example.ver2.dataClass.goalManagement.Goal;
 
 import java.util.List;
 
@@ -27,7 +29,7 @@ public class RecyclerViewTaskListAdapter extends RecyclerView.Adapter<RecyclerVi
     public TaskViewHolder onCreateViewHolder(ViewGroup parent, int viewType){
         //Task１つ分のレイアウトをViewとして読み込む
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.task_itemlist_layout, parent, false);
-        return new TaskViewHolder(view);
+        return new TaskViewHolder(view, this);
     }
 
     //ViewHolderとデータを紐づけるメソッド
@@ -70,11 +72,41 @@ public class RecyclerViewTaskListAdapter extends RecyclerView.Adapter<RecyclerVi
     public static class TaskViewHolder extends RecyclerView.ViewHolder{
         public TextView taskNameTextView;
         public TextView taskDescriptionTextView;
+        public Button taskDeleteButton;
+
+        private RecyclerViewTaskListAdapter adapter;
         //他のTaskの情報を表示するTextViewを追加
-        public TaskViewHolder(View view){
+        public TaskViewHolder(View view, RecyclerViewTaskListAdapter adapter){
             super(view);
             taskNameTextView = view.findViewById(R.id.taskName);
             taskDescriptionTextView = view.findViewById(R.id.taskDescription);
+            taskDeleteButton = view.findViewById(R.id.taskList_deleteButton);
+
+            this.adapter = adapter;
+
+            taskDeleteButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    int position = getAdapterPosition();
+                    if(position != RecyclerView.NO_POSITION && adapter != null){
+                        adapter.removeTask(position);
+                    }
+                }
+            });
+        }
+    }
+
+    //データベース自体の削除ではない（Taskはデータベースに単体で存在しているわけではない）
+    public void removeTask(int position){
+        if(position >= 0 && position < taskList.size()){
+            //クリックされたGoalオブジェクトを取得
+            Task clickedTask = taskList.get(position);
+            //そのGoalオブジェクトのIDを参照してViewModelを使用し削除（サブクラスごとに分ける）
+            if(clickedTask != null) {
+                taskList.remove(clickedTask);
+                //goalListから削除を行う
+                notifyItemRemoved(position);
+            }
         }
     }
 }
